ba487985ea40f4fa9bdb54697d914c0452699d5c
Carter Pry <clpry@pm.me>
2024-05-25 21:26:42 -0400

    Add files via upload

--- pendulum.js
+++ pendulum.js
@@ -1,69 +1,73 @@
-class Pendulum {
-    constructor(n = 2, thetas = [0.5 * Math.PI, 0.5 * Math.PI], thetaDots = [0, 0], g = 9.8) {
-        this.n = n;
-        this.thetas = thetas;
-        this.thetaDots = thetaDots;
-        this.g = g;
-    }
+let len1, len2, mass1, mass2, angle1, angle2;
+let gravity, damping;
+let aVel1 = 0, aVel2 = 0;
+let aAcc1 = 0, aAcc2 = 0;
 
-    A(thetas) {
-        const M = [];
-        for (let i = 0; i < this.n; i++) {
-            const row = [];
-            for (let j = 0; j < this.n; j++) {
-                row.push((this.n - Math.max(i, j)) * Math.cos(thetas[i] - thetas[j]));
-            }
-            M.push(row);
-        }
-        return M;
-    }
+function setup() {
+    createCanvas(800, 600);
+    len1 = select('#len1-input').value();
+    len2 = select('#len2-input').value();
+    mass1 = select('#mass1-input').value();
+    mass2 = select('#mass2-input').value();
+    angle1 = radians(select('#angle1-input').value());
+    angle2 = radians(select('#angle2-input').value());
+    gravity = select('#gravity').value();
+    damping = select('#damping').value();
+    
+    select('#len1-input').input(updateValues);
+    select('#len2-input').input(updateValues);
+    select('#mass1-input').input(updateValues);
+    select('#mass2-input').input(updateValues);
+    select('#angle1-input').input(updateValues);
+    select('#angle2-input').input(updateValues);
+    select('#gravity').input(updateValues);
+    select('#damping').input(updateValues);
+}
 
-    b(thetas, thetaDots) {
-        const v = [];
-        for (let i = 0; i < this.n; i++) {
-            let b_i = 0;
-            for (let j = 0; j < this.n; j++) {
-                b_i -= (this.n - Math.max(i, j)) * Math.sin(thetas[i] - thetas[j]) * thetaDots[j] ** 2;
-            }
-            b_i -= this.g * (this.n - i) * Math.sin(thetas[i]);
-            v.push(b_i);
-        }
-        return v;
-    }
+function draw() {
+    background(26, 26, 26);
+    translate(width / 2, height / 4);
 
-    f(thetas, thetaDots) {
-        const A = this.A(thetas);
-        const b = this.b(thetas, thetaDots);
-        return [thetaDots, math.lusolve(A, b).map(x => x[0])];
-    }
+    let num1 = -gravity * (2 * mass1 + mass2) * sin(angle1);
+    let num2 = -mass2 * gravity * sin(angle1 - 2 * angle2);
+    let num3 = -2 * sin(angle1 - angle2) * mass2;
+    let num4 = aVel2 * aVel2 * len2 + aVel1 * aVel1 * len1 * cos(angle1 - angle2);
+    let den = len1 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2));
+    aAcc1 = (num1 + num2 + num3 * num4) / den;
 
-    RK4(dt, thetas, thetaDots) {
-        const k1 = this.f(thetas, thetaDots);
-        const k2 = this.f(math.add(thetas, k1[0].map(x => 0.5 * dt * x)), math.add(thetaDots, k1[1].map(x => 0.5 * dt * x)));
-        const k3 = this.f(math.add(thetas, k2[0].map(x => 0.5 * dt * x)), math.add(thetaDots, k2[1].map(x => 0.5 * dt * x)));
-        const k4 = this.f(math.add(thetas, k3[0].map(x => dt * x)), math.add(thetaDots, k3[1].map(x => dt * x)));
+    num1 = 2 * sin(angle1 - angle2);
+    num2 = (aVel1 * aVel1 * len1 * (mass1 + mass2) + gravity * (mass1 + mass2) * cos(angle1) + aVel2 * aVel2 * len2 * mass2 * cos(angle1 - angle2));
+    den = len2 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2));
+    aAcc2 = (num1 * num2) / den;
 
-        const thetaDeltas = math.add(k1[0], k2[0].map(x => 2 * x), k3[0].map(x => 2 * x), k4[0]).map(x => x * dt / 6);
-        const thetaDotDeltas = math.add(k1[1], k2[1].map(x => 2 * x), k3[1].map(x => 2 * x), k4[1]).map(x => x * dt / 6);
+    aVel1 += aAcc1;
+    aVel2 += aAcc2;
+    aVel1 *= damping;
+    aVel2 *= damping;
+    angle1 += aVel1;
+    angle2 += aVel2;
 
-        return [math.add(thetas, thetaDeltas), math.add(thetaDots, thetaDotDeltas)];
-    }
+    let x1 = len1 * sin(angle1);
+    let y1 = len1 * cos(angle1);
+    let x2 = x1 + len2 * sin(angle2);
+    let y2 = y1 + len2 * cos(angle2);
 
-    tick(dt) {
-        [this.thetas, this.thetaDots] = this.RK4(dt, this.thetas, this.thetaDots);
-    }
+    stroke(255);
+    strokeWeight(2);
+    fill(255);
+    line(0, 0, x1, y1);
+    ellipse(x1, y1, mass1 * 5, mass1 * 5);
+    line(x1, y1, x2, y2);
+    ellipse(x2, y2, mass2 * 5, mass2 * 5);
+}
 
-    get coordinates() {
-        let x = 0;
-        let y = 0;
-        const coords = [];
-
-        for (const theta of this.thetas) {
-            x += Math.sin(theta);
-            y -= Math.cos(theta);
-            coords.push({ x, y });
-        }
-
-        return coords;
-    }
+function updateValues() {
+    len1 = select('#len1-input').value();
+    len2 = select('#len2-input').value();
+    mass1 = select('#mass1-input').value();
+    mass2 = select('#mass2-input').value();
+    angle1 = radians(select('#angle1-input').value());
+    angle2 = radians(select('#angle2-input').value());
+    gravity = select('#gravity').value();
+    damping = select('#damping').value();
 }
